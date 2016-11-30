11a9399af256638796c876d133a76062391a26d3
js456 <justins4@comcast.net>
2016-11-30 10:10:06 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        //this is the old way of doing this
+	    String MyX = "";
+        String X = "X";
+        for (int k = 0; k<8; k++){
+            MyX = MyX + X;
+            System.out.println(MyX);
+        }
+
+
+        //---------------------------------------------
+    }
+}
